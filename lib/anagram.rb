50fd08566420849fc69a22aebc494f296a611da8
nmhummel <natalie.hummel@gmail.com>
2020-10-28 20:18:25 -0500

    commit tree b504ed443cd86b12b47968c83815d38ebb8d2c3d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram #should take a word on initialization
+    attr_accessor :words
+
+    def initialize(words)
+        @words = words
+    end
+
+    def match(array)
+        arr = []
+        array.each do |string| 
+            if string.split("").sort == @words.split("").sort
+                arr << string
+            end 
+        end
+        arr
+    end
+end
+
